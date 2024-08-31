1cdb7d6c546fa66dfbdf58d0eafee4c3dd892cc1
Pablo Muñoz <pablommartinezdev@gmail.com>
2024-08-31 21:00:22 +0200

    feat: add health checks endpoints. /health and /health-ui

--- src/CleanArchitecture.Template.Api/Swagger/SwaggerGenHelpers.cs
+++ src/CleanArchitecture.Template.Api/Swagger/SwaggerGenHelpers.cs
@@ -58,5 +58,35 @@ namespace CleanArchitecture.Template.Api.Swagger
             options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
             options.OperationFilter<JwtBearerOperationFilter>();
         }
+        public class HealthChecksDocumentFilter : IDocumentFilter
+        {
+            public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+            {
+                var pathItem = new OpenApiPathItem();
+                pathItem.AddOperation(OperationType.Get, new OpenApiOperation
+                {
+                    Tags = new List<OpenApiTag> { new OpenApiTag { Name = "Health Checks" } },
+                    Summary = "Health Check Endpoint",
+                    Description = "Returns the health status of the application.",
+                    Responses = new OpenApiResponses
+            {
+                {
+                    "200", new OpenApiResponse
+                    {
+                        Description = "OK"
+                    }
+                },
+                {
+                    "503", new OpenApiResponse
+                    {
+                        Description = "Service Unavailable"
+                    }
+                }
+            }
+                });
+
+                swaggerDoc.Paths.Add("/health", pathItem);
+            }
+        }
     }
 }
