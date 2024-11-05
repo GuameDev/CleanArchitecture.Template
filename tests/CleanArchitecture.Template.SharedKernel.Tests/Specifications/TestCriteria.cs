a60e47c68b094cabd4cd6696589090e9df7c95ec
Pablo Muñoz <pablommartinezdev@gmail.com>
2024-11-05 09:34:25 +0100

    feat: tests for specification pattern

---
+++ tests/CleanArchitecture.Template.SharedKernel.Tests/Specifications/TestCriteria.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Template.SharedKernel.Specification.Criterias;
+using System.Linq.Expressions;
+namespace CleanArchitecture.Template.SharedKernel.Tests.Specifications
+{
+    public class TestCriteria<T> : Criteria<T>
+    {
+        private readonly Expression<Func<T, bool>> _expression;
+        public TestCriteria(Expression<Func<T, bool>> expression)
+        {
+            _expression = expression;
+        }
+        public override Expression<Func<T, bool>> ToExpression() => _expression;
+    }
+}
---
+++ tests/CleanArchitecture.Template.SharedKernel.Tests/Specifications/TestEntity.cs
@@ -0,0 +1,10 @@
+﻿using CleanArchitecture.Template.SharedKernel.Entities;
+namespace CleanArchitecture.Template.SharedKernel.Tests.Specifications
+{
+    public class TestEntity : Entity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+}
